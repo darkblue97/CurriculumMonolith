716b3dd2ccf45cc914485429d186902b3c0b5822
Miguel Ángel López <darkvalhalla@protonmail.ch>
2022-08-18 19:35:58 +0200

    Added skills service imp (#17)

--- src/main/java/com/darkblue97/curriculummonolith/domain/dao/impl/SkillsDAO.java
+++ src/main/java/com/darkblue97/curriculummonolith/domain/dao/impl/SkillsDAO.java
@@ -1,43 +1,85 @@
 package com.darkblue97.curriculummonolith.domain.dao.impl;
 
+import com.darkblue97.curriculummonolith.domain.Skills;
 import com.darkblue97.curriculummonolith.domain.dao.DAOInterface;
 import com.darkblue97.curriculummonolith.domain.dto.SkillsDTO;
+import com.darkblue97.curriculummonolith.exceptions.DataAlreadySavedException;
 import com.darkblue97.curriculummonolith.exceptions.NotFoundException;
 import com.darkblue97.curriculummonolith.repository.SkillsRepository;
+import com.darkblue97.curriculummonolith.utils.GenerationUUID;
 import com.darkblue97.curriculummonolith.utils.LanguageEnum;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
+@Component
 public class SkillsDAO implements DAOInterface<SkillsDTO> {
 
-    @Autowired
-    private SkillsRepository skillsRepository;
+    private final SkillsRepository skillsRepository;
+
+    public SkillsDAO(SkillsRepository skillsRepository) {
+        this.skillsRepository = skillsRepository;
+    }
 
     @Override
     public Optional<SkillsDTO> get(UUID id) {
-        return Optional.empty();
+        return skillsRepository.findById(id).map(SkillsDTO::toDto);
+    }
+
+    public Optional<SkillsDTO> get(LanguageEnum languageEnum) {
+        return skillsRepository.findByLanguageCode(languageEnum).map(SkillsDTO::toDto);
     }
 
     @Override
     public List<SkillsDTO> getAll(LanguageEnum languageEnum) {
-        return null;
+        List<SkillsDTO> skillsDTOS = new ArrayList<>();
+        skillsRepository.findAllByLanguageCode(languageEnum).iterator().forEachRemaining(
+                k -> skillsDTOS.add(SkillsDTO.toDto(k))
+        );
+
+        return skillsDTOS;
     }
 
     @Override
-    public void save(SkillsDTO skillsDTO) {
+    @Transactional
+    public void save(SkillsDTO skillsDTO) throws DataAlreadySavedException {
+        if (!getAllByLanguageCode(skillsDTO.getLanguageCode()).isEmpty()) {
+            throw new DataAlreadySavedException("Data already saved with this language code");
+        }
 
+        skillsDTO.setId(GenerationUUID.generate());
+        skillsRepository.save(SkillsDTO.toModel(skillsDTO));
     }
 
     @Override
-    public void update(SkillsDTO skillsDTO) {
+    @Transactional
+    public void update(SkillsDTO skillsDTO) throws NotFoundException {
+        SkillsDTO skillsToUpdate = get(skillsDTO.getId()).orElseThrow(() -> new NotFoundException("Data not found"));
+        skillsToUpdate.setSkillName(skillsDTO.getSkillName());
+        skillsToUpdate.setDescription(skillsDTO.getDescription());
+        skillsToUpdate.setMasteringLevel(skillsDTO.getMasteringLevel());
+        skillsToUpdate.setLanguageCode(skillsDTO.getLanguageCode());
 
+        skillsRepository.save(SkillsDTO.toModel(skillsToUpdate));
     }
 
     @Override
+    @Transactional
     public void delete(UUID id) throws NotFoundException {
+        SkillsDTO skillsDTO = get(id).orElseThrow(() -> new NotFoundException("Data not found"));
+        Skills skills = SkillsDTO.toModel(skillsDTO);
+        skillsRepository.delete(skills);
+    }
 
+    public List<SkillsDTO> getAllByLanguageCode(LanguageEnum languageEnum) {
+        List<SkillsDTO> skillsDTOS = new ArrayList<>();
+        skillsRepository.findAllByLanguageCode(languageEnum).iterator().forEachRemaining(skills -> skillsDTOS.add(SkillsDTO.toDto(skills)));
+        return skillsDTOS;
     }
 }
--- src/main/java/com/darkblue97/curriculummonolith/service/impl/SkillServiceImpl.java
+++ src/main/java/com/darkblue97/curriculummonolith/service/impl/SkillServiceImpl.java
@@ -1,10 +1,49 @@
 package com.darkblue97.curriculummonolith.service.impl;
 
+import com.darkblue97.curriculummonolith.domain.dao.impl.SkillsDAO;
+import com.darkblue97.curriculummonolith.domain.dto.SkillsDTO;
+import com.darkblue97.curriculummonolith.exceptions.DataAlreadySavedException;
+import com.darkblue97.curriculummonolith.exceptions.NotFoundException;
 import com.darkblue97.curriculummonolith.service.SkillsService;
+import com.darkblue97.curriculummonolith.utils.LanguageEnum;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Component
 @Service
 public class SkillServiceImpl implements SkillsService {
+
+    private final SkillsDAO skillsDAO;
+
+    @Override
+    public SkillsDTO getSkillInformation(LanguageEnum language) throws NotFoundException {
+        return skillsDAO.get(language).orElseThrow(() -> new NotFoundException("Data not found"));
+    }
+
+    public SkillServiceImpl(SkillsDAO skillsDAO) {
+        this.skillsDAO = skillsDAO;
+    }
+
+    @Override
+    public List<SkillsDTO> getAll(LanguageEnum languageEnum) throws NotFoundException {
+        return skillsDAO.getAll(languageEnum);
+    }
+
+    @Override
+    public void putObject(SkillsDTO skillsDTO) throws DataAlreadySavedException {
+        skillsDAO.save(skillsDTO);
+    }
+
+    @Override
+    public void postObject(SkillsDTO skillsDTO) throws NotFoundException, DataAlreadySavedException {
+        skillsDAO.update(skillsDTO);
+    }
+
+    @Override
+    public void deleteObject(UUID id) throws NotFoundException {
+        skillsDAO.delete(id);
+    }
 }
